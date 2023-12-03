e0ce0736920483aeef0f07039a425aac53f1c86b
MalkaGreenberg <malkagreenberg9@gmail.com>
2023-12-03 00:10:05 -0500

    added GitHub link

--- app/src/components/landingPage.jsx
+++ app/src/components/landingPage.jsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
             <h1>Reel Delights</h1>
             <p>Feeling social? Create a Movie Mingle Night with just a few clicks. Send calendar invites to your friends, allowing them to mark their calendars for a cinematic rendezvous. Each friend invited gets to vote on the movie of the night and share their own recommendations.</p>
             <a href="#login-signup" class="btn">Login/Signup</a>
-            <a href="https://github.com/yourusername/reeldelights" class="btn btn-github" target="_blank">Check us out on GitHub</a>
+            <a href="https://github.com/MalkaGreenberg/reel-delights" class="btn btn-github" target="_blank">Check us out on GitHub</a>
         </div>
         <div class="right-content">
             <img src={image} alt="Reel Delights Image"/>
