46eef59eeeee57b4d3acf43a1826033a35fdc0ea
BenThere <benjaminbirdsall@icloud.com>
2023-12-02 21:05:50 -0700

    added hooks

--- app/src/components/login.jsx
+++ app/src/components/login.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 
 const login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>Login Page</div>
   )
