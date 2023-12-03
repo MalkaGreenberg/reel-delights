8625357085d8bf395185754df58901a072c87853
MalkaGreenberg <malkagreenberg9@gmail.com>
2023-12-03 00:11:54 -0500

    added a comment

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -6,6 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LandingPage />
+    {/* this is temporary, until we set up App.jsx */}
+    <LandingPage />  
   </React.StrictMode>,
 )
