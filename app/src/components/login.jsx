a6622a44f2f4973de396e4ab6401801ca358e575
BenThere <benjaminbirdsall@icloud.com>
2023-12-02 21:06:19 -0700

    added html

--- app/src/components/login.jsx
+++ app/src/components/login.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const login = () => {
+const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -17,8 +17,23 @@ const login = () => {
   };
 
   return (
-    <div>Login Page</div>
-  )
-}
+    <div>
+      <h2>Login Page</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
-export default login;
+export default Login;
