4eeb062427775817a2de1980792782e6c161a0be
Anthony Littlejohn <ant05man@yahoo.com>
2023-12-04 19:57:59 -0500

    created routes for login

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import LandingPage from './components/landingPage.jsx';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* this is temporary, until we set up App.jsx */}
-    <LandingPage />  
-  </React.StrictMode>,
+  <App />
 )
