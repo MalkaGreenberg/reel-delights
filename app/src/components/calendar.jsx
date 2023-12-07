b2e0169fe5149e32ccdc59de4bae2fe45c4c2fd5
BenThere <benjaminbirdsall@icloud.com>
2023-12-06 17:31:36 -0700

    implement modal

--- app/src/components/calendar.jsx
+++ app/src/components/calendar.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root'); // Set the root element for accessibility
 
 const MyEventForm = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     const [selectedDate, setSelectedDate] = useState(null);
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
+        setModalIsOpen(false); // Close the modal after selecting a date
     };
 
     return (
@@ -15,11 +20,16 @@ const MyEventForm = () => {
             <form>
                 <div>
                     <label>Select Date:</label>
-                    <DatePicker selected={selectedDate} onChange={handleDateChange} />
+                    <button onClick={() => setModalIsOpen(true)}>Open Date Picker</button>
                 </div>
             </form>
-        </div>
-    );
+            <div>
+                <h2>Select Date</h2>
+                <DatePicker selected={selectedDate} onChange={handleDateChange} />
+            </div>
+        </Modal>
+    </div >
+  );
 };
 
 export default MyEventForm;
