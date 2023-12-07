8d95713e2add063e02720cc00c3f59316736d9bd
BenThere <benjaminbirdsall@icloud.com>
2023-12-06 17:42:36 -0700

    fix error

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LandingPage from './components/landingPage';
-import LoginPage from './components/calendar';
+import LoginPage from './components/login';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css' // CSS File
