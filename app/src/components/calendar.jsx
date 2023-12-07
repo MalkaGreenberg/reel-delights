fb4d75ff5e5c83f75c32bfe61c58b977a933b80a
BenThere <benjaminbirdsall@icloud.com>
2023-12-06 17:31:55 -0700

    add modal for date picker

--- app/src/components/calendar.jsx
+++ app/src/components/calendar.jsx
@@ -6,29 +6,36 @@ import Modal from 'react-modal';
 Modal.setAppElement('#root'); // Set the root element for accessibility
 
 const MyEventForm = () => {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [selectedDate, setSelectedDate] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
 
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
-        setModalIsOpen(false); // Close the modal after selecting a date
-    };
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    setModalIsOpen(false); // Close the modal after selecting a date
+  };
 
-    return (
+  return (
+    <div>
+      <h2>Event Details</h2>
+      <form>
         <div>
-            <h2>Event Details</h2>
-            <form>
-                <div>
-                    <label>Select Date:</label>
-                    <button onClick={() => setModalIsOpen(true)}>Open Date Picker</button>
-                </div>
-            </form>
-            <div>
-                <h2>Select Date</h2>
-                <DatePicker selected={selectedDate} onChange={handleDateChange} />
-            </div>
-        </Modal>
-    </div >
+          <label>Select Date:</label>
+          <button onClick={() => setModalIsOpen(true)}>Open Date Picker</button>
+        </div>
+      </form>
+
+      {/* Modal for Date Picker */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        contentLabel="Date Picker Modal"
+      >
+        <div>
+          <h2>Select Date</h2>
+          <DatePicker selected={selectedDate} onChange={handleDateChange} />
+        </div>
+      </Modal>
+    </div>
   );
 };
 
