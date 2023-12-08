c50953d1c0d897dc55c4857405b718f909c7630e
BrielleBroadt <brielleb.broadt@gmail.com>
2023-12-07 19:38:21 -0500

    more styling and facebook logic

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import LandingPage from './components/landingPage';
 import LoginPage from './components/login';
+// import FacebookLoginButton from './components/FacebookLogin';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css' // CSS File
+import './App.css' 
 
 
 const App = () => {
+  const handleFacebookLogin = (response) => {
+   
+    console.log(response);
+  };
   return (
     <Router>
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
+      {/* <Route path="/facebook-login" element={<FacebookLoginButton onFacebookLogin={handleFacebookLogin} />} /> */}
     </Routes>
     </Router>
+
+    
   );
+  
 }
+
+
 
 export default App;
