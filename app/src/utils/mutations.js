4b97b7cb189b83187d6605d3638203688e68b1e8
MalkaGreenberg <108021072+MalkaGreenberg@users.noreply.github.com>
2023-12-09 22:07:10 -0500

    Revert "attempted to figure out signup logic"

--- app/src/utils/mutations.js
+++ app/src/utils/mutations.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       token
       user {
         _id
@@ -13,8 +13,8 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $password: String!) {
-    addUser(username: $username, password: $password) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
